565c5b797bd6e6bac104b3fd2f6f98aa8588dc98
Rafal Wilinski <raf.wilinski@gmail.com>
2020-05-16 14:52:56 +0200

    Improve GraphQL model

--- src/graphql-server/function.ts
+++ src/graphql-server/function.ts
@@ -1,12 +1,13 @@
-import { graphql } from 'graphql';
+import { graphql, GraphQLList } from 'graphql';
 import { CloudFrontRequestEvent, Context, CloudFrontResponseCallback } from 'aws-lambda';
 import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 import { replicationRegions } from '../../dynamoDBRegions';
 
 const { DynamoDB, STS } = eval(`require('aws-sdk');`); // Do not pack with Parcel. I know, it's ugly.
 
-const edgeRegion = new STS().config.region;
 let region = replicationRegions[0];
+const edgeRegion = new STS().config.region;
+const TableName = 'ServerlessGlobalGraphqlApiDynamodbStack-globdynamodb00ADB51B-M0578CQMZCP9';
 
 if (replicationRegions.includes(edgeRegion)) {
   region = edgeRegion;
@@ -16,28 +17,52 @@ const ddb = new DynamoDB.DocumentClient({
   region,
 });
 
+const item = new GraphQLObjectType({
+  name: 'Item',
+  fields: {
+    id: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+  },
+});
+
 export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      hello: {
-        type: GraphQLString,
+      item: {
+        type: item,
+        args: {
+          id: {
+            type: GraphQLString,
+          },
+        },
+        resolve(root: any, context: any, args: any) {
+          return ddb
+            .get({
+              TableName,
+              Key: {
+                hashKey: args.id,
+              },
+            })
+            .promise()
+            .then((data: any) => data)
+            .catch((error: any) => error);
+        },
+      },
+      items: {
+        type: new GraphQLList(item),
         resolve() {
           return ddb
             .scan({
-              TableName:
-                'ServerlessGlobalGraphqlApiDynamodbStack-globdynamodb00ADB51B-M0578CQMZCP9',
+              TableName,
             })
             .promise()
-            .then((data: any) => {
-              console.log('db', { data });
-
-              return JSON.stringify(data, null, 2);
-            })
-            .catch((error: any) => {
-              console.log({ error });
-              return JSON.stringify(error, null, 2);
-            });
+            .then((data: any) => data.Items)
+            .catch((error: any) => error);
         },
       },
     },
@@ -68,11 +93,7 @@ exports.handler = (
     return callback(null, request as any);
   }
 
-  if (!request.body) {
-    return callback(null, { status: '400', body: 'Request body is missing' });
-  }
-
-  const buf = new Buffer(request.body.data as any, 'base64');
+  const buf = new Buffer(request.body!.data as any, 'base64');
   const body = JSON.parse(buf.toString());
   const query = body.query || '{ hello }';
 
